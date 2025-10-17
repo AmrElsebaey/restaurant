403d322c8e90ddb9c91243606008c24c1f20902f
Amr Elsebay <amrelsebay3@gmail.com>
2025-10-17 20:11:52 +0300

    Create Restaurant repository

---
+++ backend/src/main/java/com/elsebaey/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.elsebaey.restaurant.repositories;
+
+import com.elsebaey.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+    //TODO: custom queries
+}
