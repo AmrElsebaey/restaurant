abcd0405ee8cbd656fcdaaf31153b6b5491c43ef
Amr Elsebay <amrelsebay3@gmail.com>
2025-10-18 03:22:32 +0300

    Implementing upload & retrieve photos

---
+++ backend/src/main/java/com/elsebaey/restaurant/service/impl/PhotoServiceImpl.java
@@ -0,0 +1,36 @@
+package com.elsebaey.restaurant.service.impl;
+
+import com.elsebaey.restaurant.domain.entities.Photo;
+import com.elsebaey.restaurant.service.PhotoService;
+import com.elsebaey.restaurant.service.StorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PhotoServiceImpl implements PhotoService {
+
+    private final StorageService storageService;
+
+    @Override
+    public Photo uploadPhoto(MultipartFile file) {
+        String photoId = UUID.randomUUID().toString();
+        String url = storageService.store(file, photoId);
+
+        return Photo.builder()
+                .url(url)
+                .uploadTime(LocalDateTime.now())
+                .build();
+    }
+
+    @Override
+    public Optional<Resource> getPhotoAsResource(String id) {
+        return storageService.loadAsResource(id);
+    }
+}
