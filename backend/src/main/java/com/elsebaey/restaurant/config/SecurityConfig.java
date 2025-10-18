0cec1c1a8ccebe022bb80b8b62a51a1d7102170f
Amr Elsebay <amrelsebay3@gmail.com>
2025-10-18 19:27:49 +0300

    Implement retrieve photo endpoint

--- backend/src/main/java/com/elsebaey/restaurant/config/SecurityConfig.java
+++ backend/src/main/java/com/elsebaey/restaurant/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.elsebaey.restaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +18,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize ->
-                authorize.anyRequest().authenticated()
+                authorize
+                        .requestMatchers(HttpMethod.GET, "/api/photos/**").permitAll()
+                        .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 ->
                     oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
