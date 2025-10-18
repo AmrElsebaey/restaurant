ad56052032863e72ae57887b2257d8fd6bec0d5b
Amr Elsebay <amrelsebay3@gmail.com>
2025-10-18 18:07:12 +0300

    Handle the general exceptions

--- backend/src/main/java/com/elsebaey/restaurant/controllers/ErrorController.java
+++ backend/src/main/java/com/elsebaey/restaurant/controllers/ErrorController.java
@@ -1,6 +1,7 @@
 package com.elsebaey.restaurant.controllers;
 
 import com.elsebaey.restaurant.domain.dtos.ErrorDto;
+import com.elsebaey.restaurant.exceptions.BaseException;
 import com.elsebaey.restaurant.exceptions.StorageException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,26 @@ public class ErrorController {
         ErrorDto errorDto = ErrorDto.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .message("Unable to save or retrieve resources at this time")
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDto);
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorDto> handleBaseException(BaseException ex) {
+        log.error("Caught an unexpected exception", ex);
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An error occurred")
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDto);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleException(Exception ex) {
+        log.error("Caught an unexpected exception", ex);
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An error occurred")
                 .build();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDto);
     }
