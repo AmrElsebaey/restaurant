afb46c3dfd86e616d51084669df8accb38f4e944
Amr Elsebay <amrelsebay3@gmail.com>
2025-10-24 15:37:41 +0300

    implement restaurant service

--- backend/src/main/java/com/elsebaey/restaurant/services/RestaurantService.java
+++ backend/src/main/java/com/elsebaey/restaurant/services/RestaurantService.java
@@ -2,7 +2,18 @@ package com.elsebaey.restaurant.services;
 
 import com.elsebaey.restaurant.domain.RestaurantCreateUpdateRequest;
 import com.elsebaey.restaurant.domain.entities.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface RestaurantService {
     Restaurant createRestaurant(RestaurantCreateUpdateRequest request);
+
+    Page<Restaurant> searchRestaurants(
+            String query,
+            Float minRating,
+            Float latitude,
+            Float longitude,
+            Float radius,
+            Pageable pageable
+            );
 }
--- backend/src/main/java/com/elsebaey/restaurant/services/impl/RestaurantServiceImpl.java
+++ backend/src/main/java/com/elsebaey/restaurant/services/impl/RestaurantServiceImpl.java
@@ -9,6 +9,8 @@ import com.elsebaey.restaurant.repositories.RestaurantRepository;
 import com.elsebaey.restaurant.services.GeoLocationService;
 import com.elsebaey.restaurant.services.RestaurantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.stereotype.Service;
 
@@ -47,5 +49,28 @@ public class RestaurantServiceImpl implements RestaurantService {
                 .build();
 
         return restaurantRepository.save(restaurant);
+    }
+
+    @Override
+    public Page<Restaurant> searchRestaurants(
+            String query, Float minRating,
+            Float latitude, Float longitude,
+            Float radius, Pageable pageable) {
+
+        if (minRating != null && (query == null || query.isEmpty())) {
+            return restaurantRepository.findByAverageRatingGreaterThanEqual(minRating, pageable);
+        }
+
+        Float searchMinRating = minRating == null ? 0f : minRating;
+
+        if (query != null && !query.trim().isEmpty()) {
+            return restaurantRepository.findByQueryAndMinRating(query, searchMinRating, pageable);
+        }
+
+        if (latitude != null && longitude != null && radius != null) {
+            return restaurantRepository.findByLocationNear(latitude, longitude, radius, pageable);
+        }
+
+        return restaurantRepository.findAll(pageable);
     }
 }
