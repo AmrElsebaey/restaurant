c9206a55f1434b058e38a318900e38e5e0988fb0
Amr Elsebay <amrelsebay3@gmail.com>
2025-10-24 15:48:43 +0300

    implement restaurant summary dto

--- backend/src/main/java/com/elsebaey/restaurant/mapper/RestaurantMapper.java
+++ backend/src/main/java/com/elsebaey/restaurant/mapper/RestaurantMapper.java
@@ -4,16 +4,30 @@ import com.elsebaey.restaurant.domain.RestaurantCreateUpdateRequest;
 import com.elsebaey.restaurant.domain.dtos.GeoPointDto;
 import com.elsebaey.restaurant.domain.dtos.RestaurantCreateUpdateRequestDto;
 import com.elsebaey.restaurant.domain.dtos.RestaurantDto;
+import com.elsebaey.restaurant.domain.dtos.RestaurantSummaryDto;
 import com.elsebaey.restaurant.domain.entities.Restaurant;
+import com.elsebaey.restaurant.domain.entities.Review;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface RestaurantMapper {
     RestaurantCreateUpdateRequest toRestaurantCreateUpdateRequest(RestaurantCreateUpdateRequestDto dto);
+
     RestaurantDto toRestaurantDto(Restaurant restaurant);
+
+    @Mapping(source = "reviews", target = "totalReviews", qualifiedByName = "populateTotalReviews")
+    RestaurantSummaryDto toRestaurantSummaryDto(Restaurant restaurant);
+
+    @Named("populateTotalReviews")
+    private Integer populateTotalReviews(List<Review> reviews) {
+        return reviews.size();
+    }
 
     @Mapping(target = "latitude", source = "lat")
     @Mapping(target = "longitude", source = "lon")
