f1c580d53bd9e04fe6e590f9c0787bf10ae17760
Amr Elsebay <amrelsebay3@gmail.com>
2025-10-26 01:13:41 +0300

    Implement retrieve a list of reviews by pagination

--- backend/src/main/java/com/elsebaey/restaurant/services/ReviewService.java
+++ backend/src/main/java/com/elsebaey/restaurant/services/ReviewService.java
@@ -3,9 +3,13 @@ package com.elsebaey.restaurant.services;
 import com.elsebaey.restaurant.domain.ReviewCreateUpdateRequest;
 import com.elsebaey.restaurant.domain.entities.Review;
 import com.elsebaey.restaurant.domain.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewService {
 
     Review createReview(User author, String restaurantId, ReviewCreateUpdateRequest review);
+
+    Page<Review> listReviews(String restaurantId, Pageable pageable);
 
 }
